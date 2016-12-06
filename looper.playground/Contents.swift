805a42d11ea06d697cff83cbd9173c688ca6350c
West Kraemer <WestKraemer@gmail.com>
2016-12-06 17:30:31 -0500

    adding values to arrays

--- looper.playground/Contents.swift
+++ looper.playground/Contents.swift
@@ -2,11 +2,37 @@
 
 import UIKit
 
-var i = 1
+//var i = 1
+//
+//while i < 10 {
+//    
+//    print(i)
+//    
+//    i += 1
+//}
 
-while i < 10 {
+// display the first 20 numbers in the 7 times table
+
+//var i = 7
+//
+//while i <= 140 {
+//    
+//    print(i)
+//    
+//    i += 7
+//    
+//}
+
+var array = [7, 23, 98, 1, 0, 763]
+
+var i = 0
+
+while i < array.count {
     
-    print(i)
+    array[i] += 1 //adds 1 to the value present in the array
     
-    i += 1
+    i += 1  //adds 1 to the variable i during each loop
+    
 }
+
+print (array)
